1c85cff8d67d059dca48f19e92d063311237a26b
anakin004 <160974989+anakin004@users.noreply.github.com>
2024-12-06 17:01:00 -0500

    Update fullBuild.sh

--- fullBuild.sh
+++ fullBuild.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Step 1: Build the React app
+#building react
 echo "Building React app..."
 cd react
 npm install    # Install dependencies if not already installed
 npm run build  # Build the React app
 
-# Step 2: Copying the React build output to the Spring Boot static folder
+# copying react build into springboot static
 # -- Also copying index.html into template folder for Spring Boot
 echo "Copying React build to Spring Boot static folder..."
 cp -r build/* ../Linux-Labyrinth/src/main/resources/static/
 cp -r ../Linux-Labyrinth/src/main/resources/static/index.html ../Linux-Labyrinth/src/main/resources/templates/
 
 
-# Step 3: Build the Spring Boot app
+# building springboot
 echo "Building Spring Boot app..."
-cd ../Linux-Labyrinth  # Go to the Spring Boot project directory
-mvn clean install  # Build the Spring Boot app with Maven
+cd ../Linux-Labyrinth  
+mvn clean install
 
-# Step 4: Run the Spring Boot app
+# running springboot
 echo "Running Spring Boot app..."
-mvn spring-boot:run  # Run the Spring Boot application
+mvn spring-boot:run 
 
 echo "Build and deployment complete!"
